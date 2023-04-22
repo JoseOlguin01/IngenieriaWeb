2435f8a4d76e9f54177582688816f7209482f85f
JoseOlguin01 <80792345+JoseOlguin01@users.noreply.github.com>
2023-04-22 18:46:41 -0400

    Validación email

--- ProyectoWeb/js/contacto.js
+++ ProyectoWeb/js/contacto.js
@@ -3,13 +3,12 @@ $("#formulario").validate({
         nombre_completo:{
             required: true,
             rangelength:[3,30]
-            
         },
         e_mail:{
             required: true,
             rangelength:[4,30],
-            email:true
-
+            email:true,
+            endsWith: [".com", ".net", ".cl"]
         },
         asunto:{
             required: true,
@@ -19,32 +18,39 @@ $("#formulario").validate({
             required: true,
             maxlength: 300
         }
-
-    },messages:{
+    },
+    messages:{
         nombre_completo:{
             required: "Este campo es requerido",
-            rangelength: "El nombre debe ser de 4 a 30 caracteres",
-            
+            rangelength: "El nombre debe ser de 4 a 30 caracteres"
         },
         e_mail:{
             required: "Este campo es requerido",
             rangelength: "La contraseña debe ser de 4 a 30 caracteres",
-            email: "Lo escrito no representa un email"
+            email: "Lo escrito no representa un email",
+            endsWith: "El correo electrónico debe terminar en .com, .net o .cl"
         },
         asunto:{
             required: "Este campo es requerido",
-            maxlength: "Maximo de 35 caracteres"
+            maxlength: "Máximo de 35 caracteres"
         },
         comentarios:{
             required: "Este campo es requerido",
-            maxlength: "Limite de 300 caracteres sobrepasado"
+            maxlength: "Límite de 300 caracteres sobrepasado"
         }
-
     },
-
-
     submitHandler: function(form) {
-      form.submit();
-      alert("Comentario enviado con exito!")
+        form.submit();
+        alert("Comentario enviado con éxito!");
     }
-   });
+});
+
+
+$.validator.addMethod("endsWith", function(value, element, param) {
+    for (var i = 0; i < param.length; i++) {
+        if (value.endsWith(param[i])) {
+            return true;
+        }
+    }
+    return false;
+}, "Por favor, ingrese un correo electrónico válido que termine en .com, .net o .cl");

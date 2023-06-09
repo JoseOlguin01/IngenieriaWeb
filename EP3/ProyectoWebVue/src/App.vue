a803ae74bf46e3de87e3a237e407c567a5a13b75
JoseOlguin01 <80792345+JoseOlguin01@users.noreply.github.com>
2023-06-09 14:45:48 -0400

    Ajustes varios.
    Pagina de principal sin imágenes.

--- EP3/ProyectoWebVue/src/App.vue
+++ EP3/ProyectoWebVue/src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/header.vue';
 </script>
 
 <template>
-  <div>
-    <nav>
-      <router-link to="/index">Inicio</router-link> 
-      <router-link to="/login">Login</router-link> 
-      <router-link to="/registrar">Registrarse</router-link>
-      <router-link to="/preferencias">Preferencias</router-link> 
-      <router-link to="/contacto">Contacto</router-link> 
-    </nav>
-    <router-view/>
-      <footer> @2023 copyright</footer>
-  </div>
-
+  <Header/>
+  <router-view/>
 </template>
 
 
---
+++ EP3/ProyectoWebVue/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <router-link class="navbar-brand" to="/index"><img id="logo" src="images/logo.png" alt=""><span>HiFitness</span></router-link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <router-link class="nav-link" aria-current="page" to="/index">Inicio</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/login">Login</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/registrar">Registrarse</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/preferencias">Preferencias</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/contacto">Contactanos</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default{
+  name: 'Header'
+}
+</script>

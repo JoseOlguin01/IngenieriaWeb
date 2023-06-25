14075816ff6359765f864aaf2afb4ce18a6e8290
kevin-meza-toro <80593263+kevin-meza-toro@users.noreply.github.com>
2023-06-24 20:09:16 -0400

    Arreglo bootstrap

--- EP3/ProyectoWebVue/src/main.js
+++ EP3/ProyectoWebVue/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../src/assets/bootstrap.min.css'
+import '../src/router/bootstrap.bundle.min.js'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/js/bootstrap'
 
 createApp(App).use(router).mount('#app')
 
